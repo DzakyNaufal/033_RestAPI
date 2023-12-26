6684d671979cfc47fd8537a755dc7d84fd19522e
DzakyNaufal <dzakynmulna@gmail.com>
2023-12-26 23:16:01 +0700

    menambahkan Scaffold pada fun HomeScreen

--- app/src/main/java/com/example/consumeapi/ui/theme/home/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/consumeapi/ui/theme/home/viewmodel/HomeViewModel.kt
@@ -6,19 +6,26 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -149,12 +156,40 @@ class HomeViewModel(private val kontakRepository: KontakRepository) : ViewModel(
         override val titleRes = "Kontak"
     }
 
+    @OptIn(ExperimentalMaterial3Api::class)
     @Composable
     fun HomeScreen(
         navigateToItemEntry: () -> Unit,
         modifier: Modifier = Modifier,
         onDetailClick: (Int) -> Unit = {},
         viewModel: HomeViewModel = viewModel(factory = PenyediaViewModel.Factory)
-    ) { }
+    ) {
+        val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+        Scaffold (
+            modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+            topBar = {
+                TopAppBarKontak(
+                    title = DestinasiHome.titleRes,
+                    canNavigateBack = false,
+                    scrollBehavior = scrollBehavior,
+                )
+            },
+            floatingActionButton = {
+                FloatingActionButton(
+                    onClick = navigateToItemEntry,
+                    shape = MaterialTheme.shapes.medium,
+                    modifier = Modifier.padding(18.dp)
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Add,
+                        contentDescription = "Add Kontak"
+                    )
+                }
+            },
+        ){ innerPadding ->
+
+        }
+    }
 
 }

0eaa26aaa801b61ba0762e1326dc1636142d2d50
DzakyNaufal <dzakynmulna@gmail.com>
2023-12-26 22:29:08 +0700

    membuat fun KontakLayout

--- app/src/main/java/com/example/consumeapi/ui/theme/home/viewmodel/KontakUIState.kt
+++ app/src/main/java/com/example/consumeapi/ui/theme/home/viewmodel/KontakUIState.kt
@@ -1,19 +1,25 @@
 package com.example.consumeapi.ui.theme.home.viewmodel
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.consumeapi.model.Kontak
@@ -60,7 +66,7 @@ class HomeViewModel(private val kontakRepository: KontakRepository) : ViewModel(
             }
         }
     }
-
+    @Composable
     fun KontakCard(
         kontak: Kontak,
         onDeleteClick: (Kontak) -> Unit = {},
@@ -79,6 +85,32 @@ class HomeViewModel(private val kontakRepository: KontakRepository) : ViewModel(
                 Icon(
                     imageVector = Icons.Default.Delete,
                     contentDescription = null,
+                )
+            }
+        }
+    }
+
+    @Composable
+    fun KontakLayout(
+        kontak: List<Kontak>,
+        modifier: Modifier = Modifier,
+        onDetailClick: (Kontak) -> Unit,
+        onDeleteClick: (Kontak) -> Unit = {}
+    ) {
+        LazyColumn(
+            modifier = modifier,
+            contentPadding = PaddingValues(16.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            items(kontak) { kontak ->
+                KontakCard(
+                    kontak = kontak,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .clickable { onDetailClick(kontak) },
+                    onDeleteClick = {
+                        onDeleteClick(kontak)
+                    }
                 )
             }
         }

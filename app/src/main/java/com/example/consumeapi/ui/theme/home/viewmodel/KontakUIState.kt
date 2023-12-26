58aed1bf8b1f30f839665010ae48170fcbdafec1
DzakyNaufal <dzakynmulna@gmail.com>
2023-12-26 22:14:30 +0700

    membuat row pada fun KontakCard

--- app/src/main/java/com/example/consumeapi/ui/theme/home/viewmodel/KontakUIState.kt
+++ app/src/main/java/com/example/consumeapi/ui/theme/home/viewmodel/KontakUIState.kt
@@ -1,8 +1,18 @@
 package com.example.consumeapi.ui.theme.home.viewmodel
 
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -52,10 +62,25 @@ class HomeViewModel(private val kontakRepository: KontakRepository) : ViewModel(
     }
 
     fun KontakCard(
-        kontak: Kontak
+        kontak: Kontak,
         onDeleteClick: (Kontak) -> Unit = {},
         modifier: Modifier = Modifier
     ) {
-
+        Row (
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = kontak.email,
+                style = MaterialTheme.typography.titleMedium
+            )
+            Spacer(Modifier.weight(1f))
+            IconButton(onClick = { onDeleteClick(kontak) }) {
+                Icon(
+                    imageVector = Icons.Default.Delete,
+                    contentDescription = null,
+                )
+            }
+        }
     }
 }

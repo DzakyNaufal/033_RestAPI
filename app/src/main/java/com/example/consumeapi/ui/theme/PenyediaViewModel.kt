1617a2bbe653861e2ac5580e749b0941fd72210d
DzakyNaufal <dzakynmulna@gmail.com>
2023-12-26 21:30:23 +0700

    membuat project consume api

---
+++ app/src/main/java/com/example/consumeapi/ui/theme/PenyediaViewModel.kt
@@ -0,0 +1,22 @@
+package com.example.consumeapi.ui.theme
+
+import android.text.Spannable.Factory
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.consumeapi.KontakApplication
+import com.example.consumeapi.ui.theme.home.viewmodel.HomeViewModel
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+
+        initializer {
+            HomeViewModel(aplikasiKontak().container.kontakRepository)
+        }
+    }
+}
+
+fun CreationExtras.aplikasiKontak(): KontakApplication =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as KontakApplication)

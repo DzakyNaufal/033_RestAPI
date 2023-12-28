ff64027bdc977378851d498849ff789394d58764
DzakyNaufal <dzakynmulna@gmail.com>
2023-12-28 07:57:15 +0700

    membuat class class InsertViewModel

---
+++ app/src/main/java/com/example/consumeapi/ui/kontak/viewmodel/InsertViewModel.kt
@@ -0,0 +1,26 @@
+package com.example.consumeapi.ui.kontak.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import com.example.consumeapi.repository.KontakRepository
+
+class InsertViewModel (private val kontakRepository: KontakRepository) : viewModel() {
+
+    var insertKontakState by mutableStateOf(InsertUiState())
+        private set
+
+    fun updateInsertKontakState(insertUiEvent: InsertUiEvent) {
+        insertKontakState = InsertUiState(insertUiEvent = insertUiEvent)
+    }
+
+    suspend fun insertKontak() {
+        viewModelScope.launch {
+            try {
+                kontakRepository.insertKontak(insertKontakState.insertUiEvent.toKontak())
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+}
